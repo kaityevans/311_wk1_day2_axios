4355acfe6cb1a5f7a1f17981d0b05daaa4aa99e3
unknown <kaitlandevans@gmail.com>
2022-06-02 21:02:55 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 // import axios here
+const axios = require('axios')
+
+require('dotenv').config()
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const api_key = process.env.API_KEY
+
+
+
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  axios(url).then(res => console.log(res.data.results[0].title))
+  return axios(url)
 }
+discoverMovie()
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios(url).then(res => console.log(res.data.title))
+  return res.data
 }
+getMovieById(500)
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
